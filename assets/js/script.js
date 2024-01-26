6abb8692187298e20a2d9954d76db8ce6930f84f
Abdulla Hussain Mondal <34679678+abdulla-hussain@users.noreply.github.com>
2024-01-26 16:45:17 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Designer","Developer","Freelancer"],
+        strings: ["Html Developer","Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Designer","Developer","Freelancer"],
+        strings: ["Html Developer","Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
