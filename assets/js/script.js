5e7f7cba85f41d2a366977c5057caf1269b8ee95
Abdulla Hussain Mondal <34679678+abdulla-hussain@users.noreply.github.com>
2025-04-07 14:55:49 +0530

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["UI/UX Designer","Html Developer","Freelancer"],
+        strings: ["UI/UX Designer","Html Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["UI/UX Designer","Html Developer","Freelancer"],
+        strings: ["UI/UX Designer","Html Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
